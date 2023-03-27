0c34acc7f0b4346a9eb4f7482fff5f9112aae634
André <84719490+gggandre@users.noreply.github.com>
2023-03-27 16:37:40 -0600

    ejercico 2 covid19

---
+++ Roberto/front/ejercicio1/covid-19/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Titulo from './components/Titulo';
+
+function App() {
+  return (
+    <div className="App">
+      <Titulo texto="COVID-19"/>
+    </div>
+  );
+}
+
+export default App;
